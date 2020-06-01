50873352cf604414ea31b168d3468189ddec3497
Carlos Wimmer <coiote777@gmail.com>
2020-05-31 21:16:20 -0300

    Show errors into inputs

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiMail, FiUser, FiLock, FiArrowLeft } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import getValidationError from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -10,8 +13,28 @@ import Button from '../../components/Button';
 import { Container, Background, Content } from './styles';
 
 const SignUp: React.FC = () => {
-  const handleSubmit = useCallback((data) => {
-    console.log(data);
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationError(err);
+
+      formRef.current?.setErrors(errors);
+    }
   }, []);
 
   return (
@@ -20,14 +43,14 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
-          <Input icon={FiUser} name="name" placeholder="Nome" />
-          <Input icon={FiMail} name="email" placeholder="E-mail" />
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
           <Input
             type="password"
-            icon={FiLock}
             name="password"
+            icon={FiLock}
             placeholder="Senha"
           />
 
