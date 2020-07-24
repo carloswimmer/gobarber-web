a1a943f370eb7ebe0a0166e51e6937ab39c9cf87
Carlos Wimmer <coiote777@gmail.com>
2020-07-24 06:03:00 -0300

    Create Toast hook

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,11 +3,14 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+
 import getValidationError from '../../utils/getValidationErrors';
-import logoImg from '../../assets/logo.svg';
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 
 interface SignInFormData {
@@ -19,6 +22,7 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useAuth();
+  const { addToast } = useToast();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
@@ -36,7 +40,7 @@ const SignIn: React.FC = () => {
           abortEarly: false,
         });
 
-        signIn({
+        await signIn({
           email: data.email,
           password: data.password,
         });
@@ -47,10 +51,10 @@ const SignIn: React.FC = () => {
           formRef.current?.setErrors(errors);
         }
 
-        // dispara um toast
+        addToast();
       }
     },
-    [signIn],
+    [signIn, addToast],
   );
 
   return (
