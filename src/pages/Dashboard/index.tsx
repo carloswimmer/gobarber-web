46364d99b2fddd7584d9c39a9af94c07ac4d6eb5
Carlos Wimmer <coiote777@gmail.com>
2020-07-28 22:07:46 -0300

    Handle autenticated routes

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => (
+  <Container>
+    <h1>The Best Dashboard</h1>
+    <h2>that you have ever seen</h2>
+  </Container>
+);
+
+export default Dashboard;
