c0aa8360a5e59c287252615b3d54f04cb9f520a9
Carlos Wimmer <coiote777@gmail.com>
2020-10-16 11:31:25 -0300

    Test localstorage been read first

--- src/__tests__/hooks/auth.spec.tsx
+++ src/__tests__/hooks/auth.spec.tsx
@@ -18,6 +18,32 @@ describe('Auth hook', () => {
 
     apiMock.onPost('sessions').reply(200, apiResponse);
 
+    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    result.current.signIn({
+      email: 'johndoe@example.com.br',
+      password: '123456',
+    });
+
+    await waitForNextUpdate();
+
+    expect(result.current.user.email).toEqual('johndoe@example.com.br');
+  });
+
+  it('should try to set response on localstorage', async () => {
+    const apiResponse = {
+      user: {
+        id: 'user-123',
+        name: 'John Doe',
+        email: 'johndoe@example.com.br',
+      },
+      token: 'token-123',
+    };
+
+    apiMock.onPost('sessions').reply(200, apiResponse);
+
     const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
 
     const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
@@ -39,6 +65,27 @@ describe('Auth hook', () => {
       '@GoBarber:user',
       JSON.stringify(apiResponse.user),
     );
+  });
+
+  it('should restore saved data from storage when auth inits', () => {
+    jest.spyOn(Storage.prototype, 'getItem').mockImplementation((key) => {
+      switch (key) {
+        case '@GoBarber:token':
+          return 'token-123';
+        case '@GoBarber:user':
+          return JSON.stringify({
+            id: 'user-123',
+            name: 'John Doe',
+            email: 'johndoe@example.com.br',
+          });
+        default:
+          return null;
+      }
+    });
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
 
     expect(result.current.user.email).toEqual('johndoe@example.com.br');
   });
